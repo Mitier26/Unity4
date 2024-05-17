4bc18d9370759623fc393557b5d5eb46fba91c9d
Molkang <mitier26@gmail.com>
2024-05-17 23:12:15 +0900

    플렛포머

---
+++ Platformer/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D myBody;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private bool isGrounded;
+    private bool jumped;
+
+    private float jumpPower = 12f;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        CheckIfGrounded();
+        PlayerJump();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerWalk();
+    }
+    void PlayerWalk()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if (h > 0)
+        {
+            myBody.velocity = new Vector2(speed, myBody.velocity.y);
+            //spriteRenderer.flipX = false;
+
+            ChangeDirection(1);
+        }
+        else if (h < 0)
+        {
+            myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+            //spriteRenderer.flipX = true;
+
+            ChangeDirection(-1);
+        }
+        else
+        {
+            myBody.velocity = new Vector2(0f, myBody.velocity.y);
+        }
+
+        anim.SetInteger("Speed", Mathf.Abs((int)myBody.velocity.x));
+    }
+
+    void ChangeDirection(int derection)
+    {
+        // localScale에 직접 값을 입력할 수 없다.
+        Vector3 tempScale = transform.localScale;
+        tempScale.x = derection;
+        transform.localScale = tempScale;
+    }
+    
+    void CheckIfGrounded()
+    {
+        isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.5f, groundLayer);
+
+        if (isGrounded)
+        {
+            if (jumped)
+            {
+                jumped = false;
+
+                anim.SetBool("Jump", false);
+            }
+        }
+    }
+
+    void PlayerJump()
+    {
+        if (isGrounded)
+        {
+            if(Input.GetKey(KeyCode.Space))
+            {
+                jumped = true;
+                myBody.velocity = new Vector2(myBody.velocity.x, jumpPower);
+
+                anim.SetBool("Jump", true);
+            }
+        }
+    }
+}
