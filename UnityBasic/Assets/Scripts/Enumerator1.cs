b760590f11e687a884205c8d0e3bf168fa890377
Molkang <mitier26@gmail.com>
2024-11-13 15:40:39 +0900

    Enumerator

---
+++ UnityBasic/Assets/Scripts/Enumerator1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Enumerator1 : MonoBehaviour
+{
+    // Enumerator : 열거자
+    // 데이터 요소를 하나씩 리턴 하는 기능
+    // C#에서는 IEnumerator 인터페이스를 이용해서 구현할 수 있다.
+
+    private void Start()
+    {
+        int[] list = new int[5] { 1, 2, 3, 4, 5 };
+
+        foreach (var one in list)
+            Debug.Log(one);
+    }
+}
---
+++ UnityBasic/Assets/Scripts/Enumerator2.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Enumerator2 : MonoBehaviour
+{
+    public class MyList
+    {
+        public enumerator GetEnumerator()
+        {
+            enumerator enumerator = new enumerator();
+            return enumerator;
+        }
+
+        public class enumerator
+        {
+            int[] nums = new int[5] { 1, 2, 3, 4, 5 };
+            int index = -1;
+
+            public object Current {  get { return nums[index]; } }
+            // 현재 요소 반환
+
+            // 다음 요소로 이동, 이동이 성공하면 true, 더 이상 이동 할 수 없으면 false
+            public bool MoveNext()
+            {
+                if (index == nums.Length - 1)
+                    return false;
+
+                index++;
+                return index < nums.Length;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        MyList mylist = new MyList();
+
+        MyList.enumerator e = mylist.GetEnumerator();
+
+        while (e.MoveNext()) Debug.Log(e.MoveNext());
+
+        //foreach(var one in mylist)
+        //    Debug.Log(one.ToString());
+    }
+}
